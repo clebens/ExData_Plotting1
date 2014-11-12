ee6683cee9760a9ead7739dc31e269aefd205943
Colin Lebens <colin.lebens@gmail.com>
2014-11-12 15:43:22 -0800

    Initial Commit

---
+++ proj1.R
@@ -0,0 +1,28 @@
+rowsToSkip <- 66637
+rowsToRead <- (60*24*2)
+
+readSelectionAndHeaders <- function(file, skipRows, n, sep, na.str="NA") {
+  classes <- c(rep("character", times=2), rep("numeric", times=7))
+  
+  headers <- c(t(read.table(file,
+                      sep=";",
+                      nrows=1,
+                      header=FALSE)))
+
+  data <- read.table(file, 
+                        sep =";", 
+                        skip=skipRows,
+                        nrows=n,
+                        na.strings = "?",
+                        colClasses = classes,
+                        header=FALSE)
+  
+  names(data) <- headers
+  data
+}
+
+# Read and Format the Data into a nice format...
+
+powerCons <- readSelectionAndHeaders("./household_power_consumption.txt", rowsToSkip, rowsToRead, ";")
+powerCons$DateTime <- as.POSIXct(paste(powerCons$Date, powerCons$Time), format="%d/%m/%Y %H:%M:%S")
+powerCons <- subset(powerCons, select = -c(1, 2))
