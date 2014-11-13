0e490b6bcebaf24379e3c72fb4c2fd1360d960e6
Colin Lebens <colin.lebens@gmail.com>
2014-11-12 16:03:55 -0800

    Finished first plot

--- loadData.R
+++ loadData.R
@@ -1,26 +1,24 @@
-function(file, skipRows, n, sep, na.str="NA") {
+loadData <- function() {
   classes <- c(rep("character", times=2), rep("numeric", times=7))
-  
-  headers <- c(t(read.table(file,
-                      sep=";",
-                      nrows=1,
-                      header=FALSE)))
+file = "./household_power_consumption.txt"
+skipRows = 66637
+n = (60*24*2)
+sep = ";"
+headers <- c(t(read.table(file,
+                    sep=";",
+                    nrows=1,
+                    header=FALSE)))
 
-  data <- read.table(file, 
-                        sep =";", 
-                        skip=skipRows,
-                        nrows=n,
-                        na.strings = "?",
-                        colClasses = classes,
-                        header=FALSE)
-  
-  names(data) <- headers
-  data
+data <- read.table(file, 
+                      sep =";", 
+                      skip=skipRows,
+                      nrows=n,
+                      na.strings = "?",
+                      colClasses = classes,
+                      header=FALSE)
+
+names(data) <- headers
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+data <- subset(data, select = -c(1, 2))
+data
 }
-
-# Read and Format the Data into a nice format...
-
-powerCons <- readSelectionAndHeaders("./household_power_consumption.txt", rowsToSkip, rowsToRead, ";")
-powerCons$DateTime <- as.POSIXct(paste(powerCons$Date, powerCons$Time), format="%d/%m/%Y %H:%M:%S")
-powerCons <- subset(powerCons, select = -c(1, 2))
-powerCons
--- plot1.R
+++ plot1.R
@@ -1,1 +1,15 @@
-source("./proj1.R")
+# Load Data
+source("./loadData.R")
+powerCons <- loadData()
+
+png('plot1.png',
+    height = 480,
+    width = 480,
+    units = "px")
+
+hist(powerCons$Global_active_power, 
+     col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
+dev.off()
