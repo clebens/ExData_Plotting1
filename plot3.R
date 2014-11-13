265876e9745d35be69b010d09f82cfe37f102129
Colin Lebens <colin.lebens@gmail.com>
2014-11-12 16:52:38 -0800

    Finished plot 4

---
+++ plot3.R
@@ -0,0 +1,29 @@
+# Load Data
+source("./loadData.R")
+powerCons <- loadData()
+
+png('plot3.png',
+     height = 480,
+     width = 480,
+     units = "px")
+
+plot(powerCons$DateTime, 
+          powerCons$Sub_metering_1, 
+          xlab = "",
+          ylab = "Global Active Power (kilowatts)",
+          type="l",
+          col="black")
+
+lines(powerCons$DateTime, 
+      powerCons$Sub_metering_2, type="l", col="red")
+
+
+lines(powerCons$DateTime, 
+      powerCons$Sub_metering_3, type="l", col="blue")
+
+legend(x ="topright", 
+       legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col=c("black", "red", "blue"),
+       lwd=1)
+      
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,55 @@
+# Load Data
+source("./loadData.R")
+powerCons <- loadData()
+
+png('plot4.png',
+     height = 480,
+     width = 480,
+     units = "px")
+
+par (mfrow = c(2, 2))
+
+with(powerCons, {
+  plot(powerCons$DateTime, 
+       powerCons$Global_active_power, 
+       xlab = "",
+       ylab = "Global Active Power (kilowatts)",
+       type="l",
+       col="black")
+  plot(powerCons$DateTime, 
+       powerCons$Voltage, 
+       xlab = "",
+       ylab = "Voltage",
+       type="l",
+       col="black")
+
+  
+    plot(powerCons$DateTime, 
+        powerCons$Sub_metering_1, 
+        xlab = "datetime",
+        ylab = "Global Active Power (kilowatts)",
+        type="l",
+        col="black")
+   
+    lines(powerCons$DateTime, 
+         powerCons$Sub_metering_2, type="l", col="red")
+   
+   
+    lines(powerCons$DateTime, 
+         powerCons$Sub_metering_3, type="l", col="blue")
+   
+    legend(x ="topright", 
+          legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+          col=c("black", "red", "blue"),
+          lwd=1,
+          bty = "n")
+  
+    plot(powerCons$DateTime, 
+       powerCons$Global_ractive_power, 
+       xlab = "datetime",
+       ylab = "Global_ractive_power",
+       type="l",
+       col="black")
+})
+      
+dev.off()
